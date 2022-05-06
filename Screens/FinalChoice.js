d636c2f6195cf38b0fc5f0bb1c4a9de2b7ee52d5
TheRealEn2o <carterlorenzo1@gmail.com>
2022-05-06 03:15:45 -0400

    Added check if there are no results

--- Screens/FinalChoice.js
+++ Screens/FinalChoice.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, FlatList, StyleSheet, Image, Alert } from 'react-native'
 
 const FinalChoice = ({route, navigation}) => {
+    //https://developers.google.com/maps/documentation/places/web-service/details
   return (
     <Text>Final Choice: {route.params.name}</Text>
   )
